2d1f1368739a9b79ddaf06d509e4c389b0bdac0b
Sakib Biswas <sakibsakib99880@gmail.com>
2025-08-26 09:58:55 +0600

    baseapi add

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -2,14 +2,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { RootState } from '../app/store'
 
-// ✅ Use environment variable
+//  Use environment variable
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string
 
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     baseUrl: API_BASE_URL,
-    credentials: 'include', // ✅ must match backend
+    credentials: 'include', //  must match backend
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token
       if (token) headers.set('authorization', `Bearer ${token}`)
