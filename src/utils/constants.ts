5e61a80177b18bcb894e207fababca871220e6da
Sakib Biswas <sakibsakib99880@gmail.com>
2025-08-26 10:09:38 +0600

    constants add

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,3 @@
-// export const API_BASE_URL = 'http://localhost:5000'
-// export const DATE_FMT = { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' } as const
-
-
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1";
 
 export const DATE_FMT = { 
